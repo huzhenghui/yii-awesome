104ed9585d2d6a2e3379b9df8b609be6811c0629
胡争辉 <hu@daonao.com>
2022-02-03 15:36:02 +0800

    based app-basic-wrap/layout

---
+++ app-basic-wrap/index/config/console.php
@@ -0,0 +1,4 @@
+<?php
+$config = require __DIR__ . '/../vendor/yiisoft/yii2-app-basic/config/console.php';
+$config['basePath'] = dirname(__DIR__);
+return $config;
---
+++ app-basic-wrap/index/views/layouts/main.php
@@ -0,0 +1,45 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use app\assets\AppAsset;
+use yii\bootstrap4\Breadcrumbs;
+use yii\bootstrap4\Html;
+
+use huzhenghui\app_basic_wrap_index\widgets\Header;
+use huzhenghui\app_basic_wrap_index\widgets\Footer;
+use huzhenghui\app_basic_wrap_index\widgets\Alert;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>" class="h-100">
+
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <?php $this->registerCsrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+
+<body class="d-flex flex-column h-100">
+    <?php $this->beginBody() ?>
+    <?= Header::widget() ?>
+    <main role="main" class="flex-shrink-0">
+        <div class="container">
+            <?= Breadcrumbs::widget([
+                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+            ]) ?>
+            <?= Alert::widget() ?>
+            <?= $content ?>
+        </div>
+    </main>
+    <?= Footer::widget() ?>
+    <?php $this->endBody() ?>
+</body>
+
+</html>
+<?php $this->endPage() ?>
