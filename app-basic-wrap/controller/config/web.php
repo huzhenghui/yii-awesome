95476f508dfdbce2dca7ec684bc25a2c1726b85e
胡争辉 <hu@daonao.com>
2022-02-03 09:12:18 +0800

    add app-basic-wrap/controller

---
+++ app-basic-wrap/controller/config/web.php
@@ -0,0 +1,6 @@
+<?php
+$config = require __DIR__ . '/../vendor/yiisoft/yii2-app-basic/config/web.php';
+$config['basePath'] = dirname(__DIR__);
+$config['components']['request'] = require __DIR__ . '/../config/cookieValidationKey.local.php';
+$config['controllerNamespace'] = 'huzhenghui\app_basic_wrap_controller\controllers';
+return $config;
---
+++ app-basic-wrap/controller/src/controllers/SiteController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace huzhenghui\app_basic_wrap_controller\controllers;
+
+use app\controllers\SiteController as AppSiteController;
+
+class SiteController extends AppSiteController
+{
+}
