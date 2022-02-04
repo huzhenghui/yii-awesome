b7710bfbb7f11a5e35bb32bcee9f82978297497d
胡争辉 <hu@daonao.com>
2022-02-04 21:26:50 +0800

    add user

--- app-basic-wrap/package/config/web.php
+++ app-basic-wrap/package/config/web.php
@@ -5,4 +5,5 @@ $config['components']['request']['cookieValidationKey'] = (require __DIR__ . '/.
 $config['modules']['basiclayout']['class'] = 'huzhenghui\yii2\app_basic\layout\Module';
 $config['modules']['basicsite']['class'] = 'huzhenghui\yii2\app_basic\site\Module';
 $config['defaultRoute'] = 'basicsite/site/index';
+$config['components']['user']['identityClass'] = 'huzhenghui\yii2\app_basic\site\models\User';
 return $config;
