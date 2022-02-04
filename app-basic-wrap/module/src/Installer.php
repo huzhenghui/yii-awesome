465de3326025ba96e5feb6a1efbc4fdb9930c797
胡争辉 <hu@daonao.com>
2022-02-04 09:23:08 +0800

    add app-basic-wrap/module

---
+++ app-basic-wrap/module/src/Installer.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace huzhenghui\app_basic_wrap_module;
+
+use Composer\Installer\LibraryInstaller;
+use Composer\Script\Event;
+
+use yii\composer\Installer as YiiComposerInstaller;
+
+class Installer extends LibraryInstaller
+{
+
+    /**
+     * @param Event $event
+     */
+    public static function postUpdate($event): void
+    {
+        YiiComposerInstaller::copyFiles(array(
+            "./config/cookieValidationKey.dist.php" => "./config/cookieValidationKey.local.php",
+            "./vendor/yiisoft/yii2-app-basic/yii" => "./yii",
+            "./vendor/yiisoft/yii2-app-basic/web/index.php" => "./web/index.php",
+            "./vendor/yiisoft/yii2-app-basic/web/assets/.gitignore" => "./web/assets/.gitignore",
+        ));
+    }
+    /**
+     * @param Event $event
+     */
+    public static function postInstall($event): void
+    {
+        YiiComposerInstaller::generateCookieValidationKey("config/cookieValidationKey.local.php");
+    }
+}
