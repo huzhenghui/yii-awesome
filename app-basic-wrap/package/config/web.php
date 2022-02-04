7d043cb0535783f3205aa6065e1f8ca572375d29
胡争辉 <hu@daonao.com>
2022-02-04 12:37:49 +0800

    based app-basic-wrap/web

---
+++ app-basic-wrap/package/config/web.php
@@ -0,0 +1,8 @@
+<?php
+$config = require __DIR__ . '/../vendor/yiisoft/yii2-app-basic/config/web.php';
+$config['basePath'] = dirname(__DIR__);
+$config['components']['request']['cookieValidationKey'] = (require __DIR__ . '/../config/cookieValidationKey.local.php')['cookieValidationKey'];
+$config['modules']['basiclayout']['class'] = 'huzhenghui\yii2\app_basic\layout\Module';
+$config['modules']['basicsite']['class'] = 'huzhenghui\yii2\app_basic\site\Module';
+$config['defaultRoute'] = 'basicsite/site/index';
+return $config;
